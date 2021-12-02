23e2e658e773b733e32aea1ea3391315ead09b0d
Michał Zaród <zarodm@gmail.com>
2021-12-02 10:52:25 +0100

    initial commit | setup environment and build a test SimpleCollectible contract

---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SimpleCollectible is ERC721URIStorage {
+    uint256 public tokenCounter;
+    constructor () public ERC721 ("Dogie", "DOG"){
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newItemId;
+    }
+
+}
